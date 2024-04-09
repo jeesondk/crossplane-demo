f3650fe7b828ef8b26e6a51bcb5e777736733f71
Jesper Jensen <jsjs@lundbeck.com>
2024-04-09 08:11:05 +0200

    feat: updated services

--- src/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ src/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HLU")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+104a79a3d5914f437eb27e995893d0362ecbd7ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f99c78fb07f4128f1ef5b16cf50b6ac100589f9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
