38ae816e06ad781a6a8d2dd252e300164e0d264d
Jesper Jensen <jsjs@lundbeck.com>
2024-04-12 12:29:47 +0200

    demo: Deploying stuff

--- src/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ src/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HLU")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f99c78fb07f4128f1ef5b16cf50b6ac100589f9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023589ea8f861acfe8d4c98a4a2c338727b438e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
